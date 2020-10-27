c8f64ca23ef7d2b37dd2e5f1f2384345735f6666
Bui The Hoa <buithehoa@gmail.com>
2020-10-27 16:18:04 +0700

    Install and configure chartkick.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'charts#best_seller_products'
+
   get 'charts/best_seller_products'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
