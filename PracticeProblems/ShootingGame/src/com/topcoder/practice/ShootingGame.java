23b6380db54ad8ed9e88bfa39581573be8a24dc0
Bui The Hoa <buithehoa@gmail.com>
2024-02-02 14:29:25 +0700

    Add skeleton code.

---
+++ PracticeProblems/ShootingGame/src/com/topcoder/practice/ShootingGame.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.topcoder.practice;
+
+/**
+ * 
+ */
+public class ShootingGame {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    
+    public double findProbability(int p) {
+        
+        return -1;
+    }
+}
