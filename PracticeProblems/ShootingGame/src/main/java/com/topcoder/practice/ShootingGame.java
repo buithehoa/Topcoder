182b3cc5bb7757943da2f5bdacadab000301d89f
Bui The Hoa <buithehoa@gmail.com>
2024-02-02 17:30:26 +0700

    Add implementation.

--- PracticeProblems/ShootingGame/src/main/java/com/topcoder/practice/ShootingGame.java
+++ PracticeProblems/ShootingGame/src/main/java/com/topcoder/practice/ShootingGame.java
@@ -8,7 +8,9 @@ package com.topcoder.practice;
  */
 public class ShootingGame {
     public double findProbability(int p) {
+        double pAlice = p / Math.pow(10, 6);
+        double pBob = pAlice / (1 - pAlice);
         
-        return -1.0;
+        return (pBob >= 1) ? -1.0 : pBob;
     }
 }
--- PracticeProblems/ShootingGame/src/test/java/com/topcoder/practice/ShootingGameTest.java
+++ PracticeProblems/ShootingGame/src/test/java/com/topcoder/practice/ShootingGameTest.java
@@ -1,6 +1,6 @@
 package com.topcoder.practice;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ShootingGameTest {
-
+    private static final double DOUBLE_DELTA = Math.pow(10, -6);
+    
     @BeforeAll
     static void setUpBeforeClass() throws Exception {
     }
@@ -27,9 +28,28 @@ class ShootingGameTest {
     }
 
     @Test
-    void test() {
-        fail(
-            "Not yet implemented");
+    void findProbability_ShouldReturnBobProbability_WhenWithGoodP() {
+        ShootingGame game = getGame();
+        double pBob = game.findProbability(400000);
+        assertEquals(0.6666666666666667, pBob, DOUBLE_DELTA);
+    }
+    
+    @Test
+    void findProbability_ShouldReturnBobProbability_WhenWithNeutralP() {
+        ShootingGame game = getGame();
+        double pBob = game.findProbability(500000);
+        assertEquals(-1.0, pBob, DOUBLE_DELTA);
+    }
+    
+    @Test
+    void findProbability_ShouldReturnError() {
+        ShootingGame game = getGame();
+        double pBob = game.findProbability(999997);
+        assertEquals(-1.0, pBob, DOUBLE_DELTA);
+    }
+
+    public ShootingGame getGame() {
+        return new ShootingGame();
     }
 
 }
